c3fce59327245210f57c2582eaec88b322517380
MelisaOnal <119419249+MelisaOnal@users.noreply.github.com>
2022-12-20 14:59:17 +0300

    melisa yeniden yazma
    @themanoftalent

---
+++ bugdosyasi.c
@@ -0,0 +1,24 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main()
+{
+    typedef union
+    {
+        int integer;
+        float ondalik;
+        char str[20];
+    }veri;
+    veri data;
+    data.integer=5;
+    data.ondalik=5.5;
+    strcpy(data.str,"melisa");
+    if(strcmp(data.str,"melisa")==0)
+    {
+        printf("Merhaba %s",data.str);
+    }
+    
+    return 0;
+}
